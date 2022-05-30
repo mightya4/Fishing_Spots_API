b627341ee8a1e450efdc06337df3837d50770e51
Akeem <akeem.mighty@gmail.com>
2022-05-30 12:20:35 -0400

    Deleted Unecessary Models in Fishing Spot App + Pushed New Migrations to Database

--- backend/fishing_spots_backend/models.py
+++ backend/fishing_spots_backend/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from authentication.models import User
 
 # Create your models here.
+
 class Address(models.Model):
     name = models.CharField(max_length=255, default="")
     street = models.CharField(max_length=255, default="")
@@ -10,16 +11,16 @@ class Address(models.Model):
     zip_code = models.IntegerField(default=0)
 
 class Customers(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, null=True, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, default="")
     last_name = models.CharField(max_length=255, default="")
-    current_location = models.CharField(blank=True, max_length=255)
+    current_location = models.CharField(max_length=255, default="")
 
 class FishingSpots(models.Model):
-    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, null=True, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, null=True, on_delete=models.CASCADE)
     isFished = models.BooleanField(default=False)
     fish_types = models.CharField(max_length=255, default="")
     rating = models.DecimalField(decimal_places=1, max_digits=2)
-    images = models.FileField()
+    images = models.FileField(null=True)
