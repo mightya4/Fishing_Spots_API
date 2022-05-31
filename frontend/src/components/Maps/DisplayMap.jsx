c079dfe02b785d005dc0aefce0908e58df2084db
Akeem <akeem.mighty@gmail.com>
2022-05-31 18:00:12 -0400

    Completed Display Map Functionality in Display Map File of Fishing Spot App Frontend

--- frontend/src/components/Maps/DisplayMap.jsx
+++ frontend/src/components/Maps/DisplayMap.jsx
@@ -1,64 +1,106 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import { useGoogleMap, useLoadScript } from '@react-google-maps/api';
+import './DisplayMap.css'
+import { GOOGLE_MAP_KEY } from '../../LocalKey';
 
-let coords = []
+
+
+
+
 const DisplayMap = (props) => {
-    const google = window.google
-    
-    let [state, setState] = useState({
-        center: { lat: props.latitude, lng: props.longitude},
-        coordsResult: []
-    })
-    
-    const parkMapLoad = async map => {
-        let request = {
-            query: "Plantation Heritage Park",
-            fields: ["name", "geometry"]
-        };
+    // const google = window.google
+    // const { isLoaded } = useGoogleMaps();
+    // let coords = []
+    // let [currentState, setCurrentState] = useState({
+    //     center: { lat: props.latitude, lng: props.longitude},
+    //     coordsResult: []
+    // })
+    var map;
+    var service;
+    var infowindow;
+    var google = window.google
+    const [currentMap, setMap] = useState()
+
+
         
-        let service = new google.maps.places.PlacesService(map);
-        debugger
-        service.nearbySearch(request, (results, status) => {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    coords.push(results[i]);
-                }
-                setState({
-                    center: results[0].geometry.location,
-                    coordsResult: coords
-                });
+        
+    //     var sydney = new google.maps.LatLng(-33.867, 151.195);
+  
+    //     infowindow = new google.maps.InfoWindow();
+      
+    // //    map = new window.google.maps.Map(document.getElementById('map'), {
+    // //        center: {lat: -34.397, lng: 150.644},
+    // //        zoom: 8
+    // //    })
+    
+    
+    //     map = new google.maps.Map(
+    //         document.getElementById('map'), {center: sydney, zoom: 15});
+      
+    //     var request = {
+    //       query: 'Museum of Contemporary Art Australia',
+    //       fields: ['name', 'geometry'],
+    //     };
+      
+    //     service = new google.maps.places.PlacesService(map);
+      
+    //     service.findPlaceFromQuery(request, function(results, status) {
+    //       if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //         // for (var i = 0; i < results.length; i++) {
+    //         //   createMarker(results[i]);
+    //         // }
+    //         map.setCenter(results[0].geometry.location);
+    //       }
+    //     });
+ 
+            window.initMap = function() {
+                 map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: props.latitude, lng: props.longitude},
+                    zoom: 15})
             }
-        });
-        
-    }
+                const script = document.createElement("script");
+                const API = GOOGLE_MAP_KEY;
+                script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&libraries=places&callback=initMap`;
+                script.async = true;
+                document.body.appendChild(script);
 
-    useEffect(() => {
-        parkMapLoad()
-      }, [])
+                // map = new google.maps.Map(document.getElementById('map'), {
+                //     center: {lat: -34.397, lng: 150.644},
+                //     zoom: 8})
+            
 
-    return ( 
+
+ 
+
+    return (
         <div>
-            <GoogleMap
-                center={state.center}
-                zoom={13}
-                onLoad={map => parkMapLoad(map)}
-                mapContainerStyle={{ height: "600px", width: "600px"}}
-            >
-                {this.state.coordsResult !== [] &&
-                    this.state.coordsResult.map(function(results, i) {
-                        return(
-                            <Marker key={i} position={results.geometry.location}>
-                                <InfoWindow
-                                    options={{ maxWidth: 300}}>
-                                        <span>{results.name}</span>
-                                    </InfoWindow>
-                            </Marker>
-                        )
-                    })}
-                
-            </GoogleMap>
+        <div id="map" style={{width: 600, height: 600}}></div>
+           <script></script> {/* <script type="text/javascript" async defer src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBfx1rumPSwaYzn8UMZWcjRr7INoLBwLLY&libraries=places&callback=${window.initMap}`}></script> */}
         </div>
-     );
+
+    )
+
+    //     isLoaded ? 
+    //     (<GoogleMap
+    //     center={currentState.center}
+    //     zoom={13}
+    //     mapContainerStyle={{ height: "600px", width: "600px"}}
+    // >
+    //     {/* {currentState.coordsResult !== [] &&
+    //         currentState.coordsResult.map(function(results, i) {
+    //             return(
+    //                 <Marker key={i} position={results.geometry.location}>
+    //                     <InfoWindow
+    //                         options={{ maxWidth: 300}}>
+    //                             <span>{results.name}</span>
+    //                         </InfoWindow>
+    //                 </Marker>
+    //             )
+    //         })} */}
+        
+    //     </GoogleMap>) : <div>Loading...</div>
+    //     );
 }
  
 export default DisplayMap;
