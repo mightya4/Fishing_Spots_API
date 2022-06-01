f034b7392c418cb604563eff0f7bad93e7e982cc
Akeem <akeem.mighty@gmail.com>
2022-06-01 10:22:50 -0400

    Added Places Service to Display Map File in Fishing Spot App Frontend

--- frontend/src/components/Maps/DisplayMap.jsx
+++ frontend/src/components/Maps/DisplayMap.jsx
@@ -8,28 +8,36 @@ import { GOOGLE_MAP_KEY } from '../../LocalKey';
 
 const DisplayMap = (props) => {
     var map;
+    var infowindow;
+    var createMarker;
     var google = window.google
+    const [searchResults, setSearchResults] = useState("")
 
-    //     var request = {
-    //       query: 'Museum of Contemporary Art Australia',
-    //       fields: ['name', 'geometry'],
-    //     };
-      
-    //     service = new google.maps.places.PlacesService(map);
-      
-    //     service.findPlaceFromQuery(request, function(results, status) {
-    //       if (status === google.maps.places.PlacesServiceStatus.OK) {
-    //         // for (var i = 0; i < results.length; i++) {
-    //         //   createMarker(results[i]);
-    //         // }
-    //         map.setCenter(results[0].geometry.location);
-    //       }
-    //     });
- 
+            //Load Map from url provided in script src
             window.initMap = function() {
-                 map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: props.latitude, lng: props.longitude},
-                    zoom: 15})
+                
+                    var current_location = new google.maps.LatLng(props.latitude, props.longitude);
+
+                    infowindow = new google.maps.InfoWindow();
+                  
+                    map = new google.maps.Map(
+                        document.getElementById('map'), {center: current_location, zoom: 15});
+                  
+                    var request = {
+                      query: {searchResults},
+                      fields: ['name', 'geometry'],
+                    };
+                  
+                    var service = new google.maps.places.PlacesService(map);
+                  
+                    service.findPlaceFromQuery(request, function(results, status) {
+                      if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        for (var i = 0; i < results.length; i++) {
+                          createMarker(results[i]);
+                        }
+                        map.setCenter(results[0].geometry.location);
+                      }
+                    });
             }
                 const script = document.createElement("script");
                 const API = GOOGLE_MAP_KEY;
@@ -37,10 +45,11 @@ const DisplayMap = (props) => {
                 script.async = true;
                 document.body.appendChild(script);
 
+
     return (
         <div>
-        <div id="map" style={{width: 600, height: 600}}></div>
-           <script></script>
+        <div id="map" style={{width: 800, height: 600}}></div>
+           {/* <script></script> */}
         </div>
 
     )
