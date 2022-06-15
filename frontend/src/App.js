8c8e75de1146cbef40bb2bbbf87c199182a1ce0c
Akeem <akeem.mighty@gmail.com>
2022-06-15 16:42:15 -0400

    Removed GetLocation Component from Fishing Spot App Frontend + Removed Reference to GetLocation Component from App File of Fishing Spot App Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import GetLocation from "./components/Maps/GetLocation";
 import GoogleMaps from "./components/Maps/GoogleMaps";
 import SearchBox from "./components/Search/SearchBox";
 
@@ -22,16 +21,13 @@ import PrivateRoute from "./utils/PrivateRoute";
 
 
 function App() {
-  const [latitude, setLatitude] = useState()
-  const [longitude, setLongitude] = useState()
   const [searchResults, setSearchResults] = useState("")
 
   return (
     <div>
       <Navbar />
       <SearchBox searchResults={searchResults} setSearchResults={setSearchResults}/>
-      <GetLocation searchResults={searchResults} setLatitude={setLatitude} setLongitude={setLongitude}/>
-      <GoogleMaps latitude={latitude} longitude={longitude} searchResults={searchResults}/>
+      <GoogleMaps />
       <Routes>
         <Route
           path="/"
